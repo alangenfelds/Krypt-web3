86d53ed67fc959c8e5fefc6c2a3389a189934a10
arturs.langenfelds@28stone.com <alangenfeld@gmail.com>
2022-10-20 11:01:20 +0300

    setup smart contract

---
+++ client/src/utils/constants.js
@@ -0,0 +1,5 @@
+import contract from './Transactions.json'
+
+export const contractAddress = '0xBa4394ad7932d44B129A89c029644b6C68a18731'
+
+export const contractAbi = contract.abi;
--- smart_contract/hardhat.config.js
+++ smart_contract/hardhat.config.js
@@ -1,6 +1,12 @@
-require("@nomicfoundation/hardhat-toolbox");
+require("@nomiclabs/hardhat-waffle");
+require('dotenv').config();
 
-/** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.17",
+  solidity: "0.8.0",
+  networks: {
+    goerli: {
+      url: process.env.API_URL,
+      accounts: [process.env.PRIVATE_KEY]
+    }
+  }
 };
