9bb209568a05fa35973524964736584d8cff11d2
arturs.langenfelds@28stone.com <alangenfeld@gmail.com>
2022-10-20 09:06:59 +0300

    updated tailwind config

--- client/tailwind.config.cjs
+++ client/tailwind.config.cjs
@@ -1,5 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+  mode: "jit",
+  darkMode: false, // or 'media' or 'class'
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
