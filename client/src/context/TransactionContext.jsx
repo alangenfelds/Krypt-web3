f6b710877d5fd555c268c5ae6dc2eb25e94fece2
arturs.langenfelds@28stone.com <alangenfeld@gmail.com>
2022-10-20 18:39:47 +0300

    wip

--- client/src/context/TransactionContext.jsx
+++ client/src/context/TransactionContext.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import { ethers } from 'ethers';
 
-import { contractAbi, contractAddress } from '../utils/constants';
+import { contractABI, contractAddress } from '../utils/constants';
 
 export const TransactionContext = React.createContext();
 
@@ -10,7 +10,7 @@ const { ethereum } = window;
 const getEthereumContract = () => {
   const provider = new ethers.providers.Web3Provider(ethereum);
   const signer = provider.getSigner();
-  const transactionContract = new ethers.Contract(contractAddress, contractAbi, signer);
+  const transactionContract = new ethers.Contract(contractAddress, contractABI, signer);
 
   // console.log({ provider, signer, transactionContract })
   return transactionContract;
@@ -19,7 +19,10 @@ const getEthereumContract = () => {
 export const TransactionProvider = ({ children }) => {
   const [currentAccount, setCurrentAccount] = useState();
   const [isLoading, setIsLoading] = useState(false);
+
   const [transactionCount, setTransactionCount] = useState(localStorage.getItem("transactionCount") ?? 0);
+  const [transactions, setTransactions] = useState([]);
+
   const [formData, setFormData] = useState({
     addressTo: '',
     amount: '',
@@ -33,6 +36,34 @@ export const TransactionProvider = ({ children }) => {
       [inputName]: e.target.value
     }));
   }
+
+  const getAllTransactions = async () => {
+    try {
+      if (ethereum) {
+        const transactionsContract = getEthereumContract();
+
+        const availableTransactions = await transactionsContract.getAllTransactions();
+
+        const structuredTransactions = availableTransactions.map((transaction) => ({
+          addressTo: transaction.receiver,
+          addressFrom: transaction.sender,
+          timestamp: new Date(transaction.timestamp.toNumber() * 1000).toLocaleString(),
+          message: transaction.message,
+          keyword: transaction.keyword,
+          amount: parseInt(transaction.amount._hex) / (10 ** 18)
+        }));
+
+        console.log(structuredTransactions);
+
+        setTransactions(structuredTransactions);
+      } else {
+        console.log("Ethereum is not present");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
 
   const checkIfWalletIsConnected = async () => {
     if (!window.ethereum) {
@@ -54,6 +85,21 @@ export const TransactionProvider = ({ children }) => {
       console.log('Error getting accounts: ', error)
     }
   }
+
+  const checkIfTransactionsExists = async () => {
+    try {
+      if (window.ethereum) {
+        const transactionsContract = getEthereumContract();
+        const currentTransactionCount = await transactionsContract.getAllTransactionCount();
+
+        window.localStorage.setItem("transactionCount", currentTransactionCount);
+      }
+    } catch (error) {
+      console.log(error);
+
+      throw new Error("No ethereum object");
+    }
+  };
 
   const connectWallet = async () => {
     try {
@@ -120,10 +166,11 @@ export const TransactionProvider = ({ children }) => {
 
   useEffect(() => {
     checkIfWalletIsConnected();
+    checkIfTransactionsExists();
   }, [])
 
   return (
-    <TransactionContext.Provider value={{ connectWallet, currentAccount, handleChange, formData, sendTransaction, isLoading }}>
+    <TransactionContext.Provider value={{ connectWallet, currentAccount, handleChange, formData, sendTransaction, isLoading, transactions }}>
       {children}
     </TransactionContext.Provider>
   )
--- client/src/utils/constants.js
+++ client/src/utils/constants.js
@@ -2,4 +2,4 @@ import contract from './Transactions.json'
 
 export const contractAddress = '0xBa4394ad7932d44B129A89c029644b6C68a18731'
 
-export const contractAbi = contract.abi;
+export const contractABI = contract.abi;
